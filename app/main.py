66952e12e4559dfb6f47aac695e03d17d1259b8f
PinsaraPerera <1pawanpinsara@gmail.com>
2024-06-11 21:35:17 +0530

    login system added

---
+++ app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.db.base import Base
+from app.db.session import engine
+from app.api.endpoints import authentication, users
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(authentication.router)
+app.include_router(users.router)
